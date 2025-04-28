5f76cf5e83722321d60488673d7a5f2c92a1596d
Kailash Nadh <kailash@nadh.in>
2025-04-28 13:16:47 +0530

    Refactor projects page, update project details, and add workflow+script to auto-update periodically.

--- scripts/update-projects.py
+++ scripts/update-projects.py
@@ -11,10 +11,6 @@ from os import getenv
 from requests.auth import HTTPBasicAuth
 
 
-def fetch_project(url):
-    pass
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("provide path to projects.yml")
@@ -22,6 +18,8 @@ if __name__ == "__main__":
     api_token = getenv("GITHUB_TOKEN", "")
     if not api_token:
         sys.exit("GITHUB_TOKEN is missing")
+
+    headers = {"Authorization": "token %s" % api_token}
 
     # Source: https://stackoverflow.com/a/1774043
     with open(sys.argv[1], "r") as stream:
@@ -35,18 +33,33 @@ if __name__ == "__main__":
             continue
         print(p["github_uri"])
 
-        headers = {"Authorization": "token %s" % api_token}
+
         url = "https://api.github.com/repos/{}".format(p["github_uri"])
         r = requests.get(url, headers=headers)
-        # auth=HTTPBasicAuth("", ""))
+
+        if r.status_code in [401, 403, 429]:
+            sys.exit("unauthorized or rate limited: {}".format(r.status_code))
+        elif r.status_code != 200:
+            print("skipping {}: bad status code: {}".format(p["github_uri"], r.status_code))
+            continue
+
         data = json.loads(r.content)
 
         # Update URLs.
         projects[i]["name"] = data["name"]
         projects[i]["url"] = "https://github.com/" + p["github_uri"]
-        projects[i]["description"] = data["description"]
+
+        if data["description"]:
+            projects[i]["description"] = data["description"]
+
+        projects[i]["language"] = data["language"]
+
+        if data["license"] and "name" in data["license"]:
+            projects[i]["license"] = data["license"]["name"]
+
+        projects[i]["topics"] = data["topics"]
         projects[i]["stars"] = data["stargazers_count"]
-        projects[i]["updated_at"] = data["updated_at"]
+        projects[i]["updated_at"] = data["pushed_at"]
 
     with open(sys.argv[1], "w") as f:
         yaml.dump(projects, f)
